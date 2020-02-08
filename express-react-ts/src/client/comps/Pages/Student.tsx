c23c0a890a6ccc9d1f0af178fea63a701377cf92
Robert Tan <robert.tan@mail.utoronto.ca>
2020-02-07 22:29:21 -0500

    Clean up canvas code

--- express-react-ts/src/client/comps/Pages/Student.tsx
+++ express-react-ts/src/client/comps/Pages/Student.tsx
@@ -1,87 +1,34 @@
 import React, { useReducer, Fragment, useState, useEffect } from "react";
 import CanvasDraw from "react-canvas-draw";
-import { ScribeSVG } from "../SubComponents/ScribeSVG";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ToastContainer, toast } from "react-toastify";
 import "!style-loader!css-loader!react-toastify/dist/ReactToastify.css";
 import { Footer } from "../SubComponents/Footer";
-import { FormGroup } from "../SubComponents/Login/FormGroup";
-import ReactDOM from "react-dom";
 
-const createToast: any = toast;
-
-type LoginState = {
-  username: string;
-  password: string;
-  repassword: string;
-  isLoading: boolean;
+type CanvasState = {
   isPortraitMode: boolean;
-  isRegisterMode: boolean;
 };
 
 function reducer(
-  state: LoginState,
-  action: { type: string; fieldName?: string; value?: string }
-): LoginState {
+  state: CanvasState,
+  action: { type: string; value: string; }
+): CanvasState {
   switch (action.type) {
-    case "field":
-      return {
-        ...state,
-        [action.fieldName]: action.value
-      };
-    case "login":
-      return {
-        ...state,
-        isLoading: true
-      };
     case "resize":
       return {
         ...state,
         isPortraitMode: parseInt(action.value) < 1024
       };
-    case "start register":
-      return {
-        ...state,
-        isRegisterMode: true,
-        isLoading: false,
-        username: "",
-        password: ""
-      };
-    case "stop register":
-      return {
-        ...state,
-        isRegisterMode: false,
-        isLoading: false,
-        username: "",
-        password: "",
-        repassword: ""
-      };
-
     default:
       throw new Error("Invalid type on action.");
   }
 }
 
-const initialState: LoginState = {
-  username: "",
-  password: "",
-  repassword: "",
-  isLoading: false,
-  isPortraitMode: false,
-  isRegisterMode: false
+const initialState: CanvasState = {
+  isPortraitMode: false
 };
 
 export const Student: React.FC = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  const {
-    username,
-    password,
-    repassword,
-    isLoading,
-    isPortraitMode,
-    isRegisterMode
-  } = state;
+  const { isPortraitMode } = state;
 
   useEffect(() => {
     const handleResize = () =>
@@ -94,29 +41,23 @@ export const Student: React.FC = () => {
     };
   });
 
-  const onSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
-    e.preventDefault(); // Disabling this will cause the POST request to fire. Also allows the form to say "Please fill this field" if the user didn't.
-    dispatch({ type: "login" });
-    createToast.success("⚠️ GAMER DETECTED!!!!!! ");
-  };
-
-  let inputRef:any;
+  let inputRef: any;
   function saveCanvas() {
-    let image:any = inputRef.canvas.drawing.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    let image: any = inputRef.canvas.drawing.toDataURL("image/png").replace("image/png", "image/octet-stream");
     window.location.href = image;
   }
 
   function clearCanvas() {
-      inputRef.clear();
+    inputRef.clear();
   }
 
   function undoCanvas() {
-      inputRef.undo();
+    inputRef.undo();
   }
 
   function handleUndo(event) {
     if (event.ctrlKey && event.keyCode === 90) {
-        inputRef.undo();
+      inputRef.undo();
     }
   }
 
@@ -126,7 +67,8 @@ export const Student: React.FC = () => {
       <button onClick={clearCanvas}>Clear</button>
       <button onClick={saveCanvas}>Save</button>
       <button onClick={undoCanvas}>Undo</button>
-      {!isPortraitMode && <Footer />}
+
+      { !isPortraitMode && <Footer /> }
     </Fragment>
   );
 };

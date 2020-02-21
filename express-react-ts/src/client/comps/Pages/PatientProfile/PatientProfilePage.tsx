4c3a2a576d92067426748c759ed879aac57d306e
marcokch <chris.marcok@gmail.com>
2020-02-20 20:27:01 -0500

    nav bar dots patientprofilepage

--- express-react-ts/src/client/comps/Pages/PatientProfile/PatientProfilePage.tsx
+++ express-react-ts/src/client/comps/Pages/PatientProfile/PatientProfilePage.tsx
@@ -30,6 +30,19 @@ const contents: contentType[] = [
   "Differential & Clinical Scores",
 ];
 
+const initNavDots = () => {
+  const container = [];
+  for (let i = 0; i < contents.length; i++) {
+    container.push(
+      <div
+        id={`patient-profile-page-dot-${i}`}
+        className="patient-profile-page-dot"
+      ></div>
+    );
+  }
+  return container;
+};
+
 export const PatientProfilePage: React.FC<PatientProfilePageProps> = ({}) => {
   const [currentPage, setCurrentPage] = useState<contentType>("Demographics");
   const [isAvatarPopup, setIsAvatarPopup] = useState(false);
@@ -38,14 +51,23 @@ export const PatientProfilePage: React.FC<PatientProfilePageProps> = ({}) => {
     const sidebarItems = document.querySelectorAll(
       ".patient-profile-page-sidebar-item"
     );
+    const navDots = document.querySelectorAll(".patient-profile-page-dot");
     sidebarItems.forEach(item => {
       item.className = "patient-profile-page-sidebar-item";
+    });
+    navDots.forEach(dot => {
+      dot.className = "patient-profile-page-dot";
     });
     const selectedPage = document.querySelector(
       `#sidebar-item-${contents.indexOf(currentPage)}`
     );
+    const selectedDot = document.querySelector(
+      `#patient-profile-page-dot-${contents.indexOf(currentPage)}`
+    );
     selectedPage.className =
       "patient-profile-page-sidebar-item patient-profile-page-sidebar-item--active";
+    selectedDot.className =
+      "patient-profile-page-dot patient-profile-page-dot--active";
   }, [currentPage]);
 
   return (
@@ -71,7 +93,12 @@ export const PatientProfilePage: React.FC<PatientProfilePageProps> = ({}) => {
             })}
           </div>
         </nav>
-        <DemographicsPage />
+        <div className="patient-profile-page-page-content">
+          <DemographicsPage />
+          <div className="patient-profile-page-dots-container">
+            {initNavDots()}
+          </div>
+        </div>
       </div>
     </>
   );

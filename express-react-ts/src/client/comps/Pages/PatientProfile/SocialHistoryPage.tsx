e858a269bd280b8429d0f6ca73c23734fae86a36
Lance <lance.oribello@mail.utoronto.ca>
2020-03-02 00:01:07 -0500

    Social History page

--- express-react-ts/src/client/comps/Pages/PatientProfile/SocialHistoryPage.tsx
+++ express-react-ts/src/client/comps/Pages/PatientProfile/SocialHistoryPage.tsx
@@ -1,8 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import "../../../scss/patient-profiles/social-history.scss";
 import { IndividualPatientProfile } from "./PatientProfilePage";
 import { useHistory } from "react-router";
+import { PatientFormInput } from "../../SubComponents/PatientProfile/PatientFormInput";
+
+function reducer(
+  state: SocialHistState,
+  action: { type: string; fieldName?: string; value?: string }
+): SocialHistState {
+  switch (action.type) {
+    case "field":
+      return {
+        ...state,
+        [action.fieldName]: action.value,
+      };
+    case "NEVER":
+      return {
+        ...state,
+        smoker: "NEVER",
+        lastTimeSmoked: "",
+        packsPerDay: "",
+      };
+    case "EX":
+      return {
+        ...state,
+        smoker: "EX",
+      };
+    case "CURRENT":
+      return {
+        ...state,
+        smoker: "CURRENT",
+      };
+    default:
+      throw new Error("Invalid type on action.");
+  }
+}
+
+type SocialHistState = {
+  work: string;
+  livingConditions: string;
+  sexualHistory : string;
+  etOH: string;
+  drinksPerWeek: string;
+  smoker: "NEVER" | "EX" | "CURRENT";
+  lastTimeSmoked: string;
+  packsPerDay: string;
+  otherSubstances: string;
+};
+
+const initialState:  SocialHistState = {
+  work: "",
+  livingConditions: "",
+  sexualHistory : "",
+  etOH: "",
+  drinksPerWeek: "",
+  smoker: "NEVER",
+  lastTimeSmoked: "",
+  packsPerDay: "",
+  otherSubstances: "",
+};
 
 export const SocialHistoryPage: IndividualPatientProfile = ({
   pageName,
@@ -10,6 +67,7 @@ export const SocialHistoryPage: IndividualPatientProfile = ({
   setCurrentPage,
   transitionDuration,
   transitionName,
+  isShowingSidebar,
   patientID
 }) => {
   const history = useHistory();
@@ -19,6 +77,35 @@ export const SocialHistoryPage: IndividualPatientProfile = ({
       history.push(`/patient/${patientID}/social`);
     }
   }, [currentPage]);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const [showingWorkCanvas, setShowingWorkCanvas] = useState(true);
+  const [showingWorkText, setShowingWorkText] = useState(false);
+
+  const [showingLivingConditionsCanvas, setShowingLivingConditionsCanvas] = useState(true);
+  const [showingLivingConditionsText, setShowingLivingConditionsText] = useState(false);
+
+  const [showingSexualHistoryCanvas, setShowingSexualHistoryCanvas] = useState(true);
+  const [showingSexualHistoryText, setShowingSexualHistoryText] = useState(false);
+
+  const [showingEtOHCanvas, setShowingEtOHCanvas] = useState(true);
+  const [showingEtOHText, setShowingEtOHText] = useState(false);
+
+  const [showingDrinksPerWeekCanvas, setShowingDrinksPerWeekCanvas] = useState(true);
+  const [showingDrinksPerWeekText, setShowingDrinksPerWeekText] = useState(false);
+
+  const [showingLastTimeSmokedCanvas, setShowingLastTimeSmokedCanvas] = useState(true);
+  const [showingLastTimeSmokedText, setShowingLastTimeSmokedText] = useState(false);
+
+  const [showingPacksPerDayCanvas, setShowingPacksPerDayCanvas] = useState(true);
+  const [showingPacksPerDayText, setShowingPacksPerDayText] = useState(false);
+
+  const [showingOtherSubstancesCanvas, setShowingOtherSubstancesCanvas] = useState(true);
+  const [showingOtherSubstancesText, setShowingOtherSubstancesText] = useState(false);
+
+  const { work, livingConditions, sexualHistory, etOH, drinksPerWeek, smoker, lastTimeSmoked, packsPerDay, otherSubstances } = state;
+
   return (
     <>
       <CSSTransition
@@ -28,71 +115,190 @@ export const SocialHistoryPage: IndividualPatientProfile = ({
         onEnter={() => setCurrentPage(pageName)}
         classNames={transitionName}
       >
-        <div className="social-history-page-outermost-container patient-profile-window">
+        <div
+          className={
+            isShowingSidebar
+              ? "patient-profile-window"
+              : "patient-profile-window width-100"
+          }
+        >
           <div className="patient-profile-page-title">
             <h1>{pageName}</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-              repellat explicabo iste neque? Fugit sint voluptate temporibus
-              vero debitis qui recusandae neque labore? Odio saepe, vel
-              voluptatum nam mollitia magnam atque nobis, explicabo rem vitae ab
-              ut neque accusantium quo eligendi in provident eos! Facere,
-              tempore distinctio non minima suscipit amet quos expedita
-              dignissimos autem aspernatur molestiae? Dolorem veniam odit iste
-              fugiat repellat quo ducimus eum, earum dolores corrupti incidunt?
-              Dolores quas architecto veritatis eius exercitationem, beatae
-              maxime laborum. Nemo, blanditiis corrupti ducimus reiciendis
-              nesciunt temporibus, natus, perferendis id non animi illo placeat
-              aliquid. Dignissimos, dolorem officiis a tempore ducimus quibusdam
-              ratione quos nostrum accusamus illo iste harum quidem eum? Nemo
-              quidem distinctio magnam iste fugit delectus illum, omnis,
-              reprehenderit, fuga quibusdam facere odio. Ad nesciunt recusandae
-              eaque iure omnis sed laudantium id dolorem beatae esse, repellat
-              temporibus et, numquam mollitia nostrum ducimus sequi est.
-              Laudantium aliquid facilis natus eaque, assumenda pariatur
-              quisquam delectus ea magnam error at odio sunt quod quidem
-              temporibus! Corporis error perspiciatis facilis, repellat tenetur
-              natus pariatur ipsam? Maxime sint labore ducimus voluptates?
-              Aspernatur placeat quisquam consequatur nobis assumenda rem sunt,
-              fugiat ut atque nisi! Repellat non doloribus dolores fuga error,
-              officia, adipisci rem corrupti corporis velit quisquam sunt nobis
-              animi eius rerum incidunt accusantium quibusdam. Accusantium at,
-              necessitatibus rerum libero, perspiciatis aspernatur sit
-              reprehenderit cum, impedit quis pariatur laboriosam voluptas
-              dolore et voluptatibus sint unde esse. Neque officiis, iure
-              ducimus optio rem ab veritatis iste sapiente inventore, id
-              necessitatibus aperiam ipsum deserunt dolores cupiditate, delectus
-              esse eaque molestiae animi explicabo qui consectetur cumque
-              aliquam quos. Ab ex recusandae eligendi similique, autem doloribus
-              dignissimos deleniti repudiandae reiciendis delectus error sed
-              odio. Id delectus tempore alias ea neque reiciendis a ut est quia
-              rerum dolores suscipit, provident explicabo excepturi totam veniam
-              quo consequuntur quisquam illum eum! Culpa, ut non. Explicabo
-              adipisci numquam perferendis dolore nemo natus vel at possimus
-              voluptas blanditiis, eos iste quasi ipsum inventore amet
-              praesentium, perspiciatis tenetur dolorem est non vero laudantium
-              debitis nobis pariatur! Ipsum, est. Enim cumque et eligendi
-              dolores laborum pariatur eius cum consectetur cupiditate animi!
-              Esse ipsam, officiis necessitatibus dolore iste exercitationem
-              sapiente voluptatem tempore illum eum optio, explicabo veniam
-              consectetur ea tenetur, nihil similique molestias blanditiis
-              facilis consequatur possimus expedita nostrum nam. Corrupti,
-              dolorem! Modi et consequatur corporis ullam sint blanditiis? Nemo
-              dolore voluptatem, corporis temporibus voluptas voluptatum odit
-              repellendus tempora commodi quia cum nesciunt vitae beatae quae
-              vel. Asperiores, minima consequuntur? Itaque architecto tenetur
-              molestias sunt cupiditate nesciunt, maiores sed obcaecati
-              asperiores odit magnam temporibus incidunt nobis assumenda eos,
-              fuga eum animi voluptatum facere in quo at tempore aspernatur.
-              Dolor laudantium impedit nostrum rerum quo at libero eius tenetur
-              dolorem non laborum ipsum tempora amet veniam sed enim quos nisi
-              adipisci placeat, nulla assumenda in inventore unde mollitia! Nisi
-              odio suscipit dolores laudantium ut omnis, illo, cumque incidunt
-              ab similique iure consequuntur placeat qui odit. Atque placeat quo
-              quidem maxime iusto ducimus minus consequuntur sequi deleniti
-              numquam exercitationem ut delectus eius possimus eligendi, quam ab
-              sapiente! Sapiente, voluptate.
-            </p>
+          </div>
+          <div className="patient-profile-form-container">
+            <PatientFormInput
+              dispatch={dispatch}
+              id={"work"}
+              inputType={"text"}
+              inputVal={work}
+              placeholder={"Enter text here"}
+              title={"Work"}
+              isShowingCanvas={showingWorkCanvas}
+              isShowingText={showingWorkText}
+              setIsShowingCanvas={setShowingWorkCanvas}
+              setIsShowingText={setShowingWorkText}
+              canvasHeight={200}
+              canvasWidth={600}
+              isTextArea={true}
+            />
+
+            <PatientFormInput
+              dispatch={dispatch}
+              id={"livingConditions"}
+              inputType={"text"}
+              inputVal={livingConditions}
+              placeholder={"Enter text here"}
+              title={"Living Conditions"}
+              isShowingCanvas={showingLivingConditionsCanvas}
+              isShowingText={showingLivingConditionsText}
+              setIsShowingCanvas={setShowingLivingConditionsCanvas}
+              setIsShowingText={setShowingLivingConditionsText}
+              canvasHeight={200}
+              canvasWidth={600}
+              isTextArea={true}
+            />
+
+            <PatientFormInput
+              dispatch={dispatch}
+              id={"sexualHistory"}
+              inputType={"text"}
+              inputVal={sexualHistory}
+              placeholder={"Enter text here"}
+              title={"Sexual History"}
+              isShowingCanvas={showingSexualHistoryCanvas}
+              isShowingText={showingSexualHistoryText}
+              setIsShowingCanvas={setShowingSexualHistoryCanvas}
+              setIsShowingText={setShowingSexualHistoryText}
+              canvasHeight={200}
+              canvasWidth={600}
+              isTextArea={true}
+            />
+
+            <h1> Substance Use: </h1> 
+
+            <PatientFormInput
+              dispatch={dispatch}
+              id={"etOH"}
+              inputType={"text"}
+              inputVal={etOH}
+              placeholder={"Enter text here"}
+              title={"EtOH"}
+              isShowingCanvas={showingEtOHCanvas}
+              isShowingText={showingEtOHText}
+              setIsShowingCanvas={setShowingEtOHCanvas}
+              setIsShowingText={setShowingEtOHText}
+              canvasHeight={200}
+              canvasWidth={600}
+              isTextArea={true}
+            />
+
+            <PatientFormInput
+              dispatch={dispatch}
+              id={"drinksPerWeek"}
+              inputType={"number"}
+              inputVal={drinksPerWeek}
+              placeholder={"Enter text here"}
+              title={"Number of Drinks/Week"}
+              isShowingCanvas={showingDrinksPerWeekCanvas}
+              isShowingText={showingDrinksPerWeekText}
+              setIsShowingCanvas={setShowingDrinksPerWeekCanvas}
+              setIsShowingText={setShowingDrinksPerWeekText}
+              canvasHeight={200}
+              canvasWidth={600}
+              isTextArea={false}
+            />
+
+            <div className="radio-group">
+              <label>
+                <input
+                  type="radio"
+                  name="smoker"
+                  checked={smoker === "NEVER"}
+                  onChange={() => {
+                    dispatch({ type: "NEVER" });
+                  }}
+                />
+                <p>Never Smoker</p>
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="smoker"
+                  checked={smoker === "EX"}
+                  onChange={() => {
+                    dispatch({ type: "EX" });
+                  }}
+                />
+                <p>Ex-Smoker</p>
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="smoker"
+                  checked={smoker === "CURRENT"}
+                  onChange={() => {
+                    dispatch({ type: "CURRENT" });
+                  }}
+                />
+                <p>Current Smoker</p>
+              </label>
+            </div>
+            {(smoker === "CURRENT" || smoker === "EX") && (
+              <>
+                <PatientFormInput
+                  dispatch={dispatch}
+                  id={"lastTimeSmoked"}
+                  inputType={"text"}
+                  inputVal={lastTimeSmoked}
+                  placeholder={"Enter text here"}
+                  title={"Last time smoked"}
+                  isShowingCanvas={showingLastTimeSmokedCanvas}
+                  isShowingText={showingLastTimeSmokedText}
+                  setIsShowingCanvas={setShowingLastTimeSmokedCanvas}
+                  setIsShowingText={setShowingLastTimeSmokedText}
+                  canvasHeight={200}
+                  canvasWidth={600}
+                  isTextArea={false}
+                />
+
+                <PatientFormInput
+                  dispatch={dispatch}
+                  id={"packsPerDay"}
+                  inputType={"text"}
+                  inputVal={packsPerDay}
+                  placeholder={"Enter text here"}
+                  title={"Packs/Day"}
+                  isShowingCanvas={showingPacksPerDayCanvas}
+                  isShowingText={showingPacksPerDayText}
+                  setIsShowingCanvas={setShowingPacksPerDayCanvas}
+                  setIsShowingText={setShowingPacksPerDayText}
+                  canvasHeight={200}
+                  canvasWidth={600}
+                  isTextArea={false}
+                />
+                
+              </>
+            )}
+            <PatientFormInput
+              dispatch={dispatch}
+              id={"otherSubstances"}
+              inputType={"text"}
+              inputVal={otherSubstances}
+              placeholder={"Enter text here"}
+              title={"Other Substances"}
+              isShowingCanvas={showingOtherSubstancesCanvas}
+              isShowingText={showingOtherSubstancesText}
+              setIsShowingCanvas={setShowingOtherSubstancesCanvas}
+              setIsShowingText={setShowingOtherSubstancesText}
+              canvasHeight={200}
+              canvasWidth={600}
+              isTextArea={true}
+            />
+          </div>
+          <div className="form-whitespace">
+            <div className="home-page-content-whitespace-logo"></div>
           </div>
         </div>
       </CSSTransition>

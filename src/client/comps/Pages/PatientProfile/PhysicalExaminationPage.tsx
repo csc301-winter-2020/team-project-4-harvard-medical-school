7e0fe234dbdd07ef66507281a16c96a4c3220831
Sanggon Choi <sanggon.choi.dc@gmail.com>
2020-03-13 19:48:29 -0400

    vitals values are there

--- src/client/comps/Pages/PatientProfile/PhysicalExaminationPage.tsx
+++ src/client/comps/Pages/PatientProfile/PhysicalExaminationPage.tsx
@@ -6,30 +6,100 @@ import { PatientFormInput } from "../../SubComponents/PatientProfile/PatientForm
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../../scss/patient-profiles/patient-physical-form.scss";
 
+function reducer(
+  state: physicalExaminationState,
+  action: {
+    category: string
+    condition_name: string
+    value: any
+    physicalExaminations: physicalExaminationState | null
+  }
+): physicalExaminationState {
+  if(action.category === 'vitalsValues'){
+    return {
+      ...state,
+      ['vitalsValues']: {
+        ...state.vitalsValues,
+        [action.condition_name]: action.value
+      }
+    }
+  }
+}
 
-type field_value = 'not_performed' | 'healthy' | 'condition'
+type fieldValue = 'not_performed' | 'healthy' | 'condition'
 
-type vitals = {[key:string]: field_value}
-type general = {[key:string]: field_value}
-type heent = {[key:string]: field_value}
-type cn_ii = {[key:string]: field_value}
-type cn_iii = {[key:string]: field_value}
-type cn_v = {[key:string]: field_value}
-type cn_vii = {[key:string]: field_value}
-type cn_viii = {[key:string]: field_value}
-type cn_ix = {[key:string]: field_value}
-type cn_xi = {[key:string]: field_value}
-type cn_xii = {[key:string]: field_value}
-type neck = {[key:string]: field_value}
-type pulmonary = {[key:string]: field_value}
-type cardiac = {[key:string]: field_value}
-type abdomen = {[key:string]: field_value}
-type skin = {[key:string]: field_value}
+type category = vitalsValues
+              | vitals
+              | general
+              | heent
+              | cn_ii
+              | cn_iii
+              | cn_v
+              | cn_vii
+              | cn_viii
+              | cn_ix
+              | cn_xi
+              | cn_xii
+              | neck
+              | pulmonary
+              | cardiac
+              | abdomen
+              | skin
+              | motor
+              | coordination
+              | gait
+              | mental
+
+type vitalsValues = {[key:string]: number | ''}
+type vitals = {[key:string]: fieldValue}
+type general = {[key:string]: fieldValue}
+type heent = {[key:string]: fieldValue}
+type cn_ii = {[key:string]: fieldValue}
+type cn_iii = {[key:string]: fieldValue}
+type cn_v = {[key:string]: fieldValue}
+type cn_vii = {[key:string]: fieldValue}
+type cn_viii = {[key:string]: fieldValue}
+type cn_ix = {[key:string]: fieldValue}
+type cn_xi = {[key:string]: fieldValue}
+type cn_xii = {[key:string]: fieldValue}
+type neck = {[key:string]: fieldValue}
+type pulmonary = {[key:string]: fieldValue}
+type cardiac = {[key:string]: fieldValue}
+type abdomen = {[key:string]: fieldValue}
+type skin = {[key:string]: fieldValue}
 // TODO : check if this is right
-type motor = {[key:string]: field_value}
-type coordination = {[key:string]: field_value}
-type gait = {[key:string]: field_value}
-type mental = {[key:string]: field_value}
+type motor = {[key:string]: fieldValue}
+type coordination = {[key:string]: fieldValue}
+type gait = {[key:string]: fieldValue}
+type mental = {[key:string]: fieldValue}
+
+type physicalExaminationState = {
+  [key:string]: category
+}
+
+const initialVitalsValues: vitalsValues = {
+  blood_pressure_systolic: '',
+  blood_pressure_diastolic: '',
+  beats_per_minute: '',
+  respiratory_rate: '',
+  temperature: '',
+  o2_saturation: '',
+  bmi: '' 
+}
+
+const initialPhysicalExaminationsState: physicalExaminationState = {
+  vitalsValues: initialVitalsValues
+}
+
+const nameMap: {[key:string]: string} = {
+  blood_pressure_systolic: 'Systolic Blood Pressure',
+  blood_pressure_diastolic: 'Diastolic Blood Pressure',
+  beats_per_minute: 'Beats Per Minute',
+  respiratory_rate: 'Respiratory Rate',
+  temperature: 'Temperature',
+  o2_saturation: 'O2 Saturation',
+  bmi: 'BMI'
+}
 
 export const PhysicalExaminationPage: IndividualPatientProfile = ({
   pageName,
@@ -48,6 +118,8 @@ export const PhysicalExaminationPage: IndividualPatientProfile = ({
     }
   }, [currentPage]);
 
+  const [state, dispatch] = useReducer(reducer, initialPhysicalExaminationsState)
+
   return (
     <>
       <CSSTransition
@@ -59,7 +131,49 @@ export const PhysicalExaminationPage: IndividualPatientProfile = ({
       >
         <div className={isShowingSidebar ? "physical-examination-history-page-outermost-container patient-profile-window" :
                                            "physical-examination-history-page-outermost-container patient-profile-window width-100"}>
-          
+          <div className="patient-profile-page-title">
+            <h2>{pageName}</h2>
+          </div>
+          <div className="patient-profile-form-container">
+            <h2>Vitals</h2><br></br>
+            <table><tbody>
+            {
+              Object.keys(state.vitalsValues).map((condition: string) => {
+                return (
+                  <tr key={condition}>
+                    <td>
+                    <label style={{
+                      fontSize: '1.2rem',
+                      marginRight: '30px'
+                    }}>{nameMap[condition]}</label>
+                    </td>
+                    <td>
+                    <input
+                      className="form-input-short"
+                      placeholder={'Put value here'}
+                      value={state.vitalsValues[condition]}
+                      type={'number'}
+                      name={condition}
+                      style={{
+                        display: 'inline-block'
+                      }}
+                      onChange={e => {
+                        console.log(e.target.value)
+                        dispatch({
+                          category: 'vitalsValues',
+                          condition_name: condition,
+                          value: e.target.value,
+                          physicalExaminations: null
+                        })
+                      }}
+                    />
+                    </td>
+                  </tr>
+                )
+              })
+            }
+            </tbody></table>
+          </div>
           <div className="patient-profile-nav-btns">
             <div className="nav-btn" style={{ right: "20px", top: "70px", position: "fixed", borderRadius: "5px" }} onClick={() => {
               // TODO : add POST request function here

07506e9461714105cb13416d85e9ee6c72bf0b07
Sanggon Choi <sanggon.choi.dc@gmail.com>
2020-03-13 18:01:45 -0400

    added types for the 3-column fields

--- src/client/comps/Pages/PatientProfile/PhysicalExaminationPage.tsx
+++ src/client/comps/Pages/PatientProfile/PhysicalExaminationPage.tsx
@@ -6,196 +6,30 @@ import { PatientFormInput } from "../../SubComponents/PatientProfile/PatientForm
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../../scss/patient-profiles/patient-physical-form.scss";
 
-type PhysicalExamVitals = {
-  bloodPressure: string;
-  heartRate: number;
-  oxygenSaturation: number;
-  temperature: number;
-  respiratoryRate: number;
-  oxygenSupplement?: string;
-  weight?: number;
-  height?: number;
-  bmi?: number;
-  painScore?: number;
-};
 
-type physicalExamDispatch = React.Dispatch<{
-  type: string;
-  bodyPart: string;
-}>;
+type field_value = 'not_performed' | 'healthy' | 'condition'
 
-// An empty string for the body part and status is allowed, and implies it may
-// be edited by a textbox/canvas (and we are in the process of figuring out
-// what the body part is).
-//
-// Further, an empty string for the status means that a textbox/canvas will be
-// placed in the appropriate cell so the user can edit it. This may need to be
-// changed in the future.
-type PhysicalExamComponent = {
-  bodyPart: string;
-  status: string;
-  custom: boolean;
-  editable: boolean;
-};
-
-type PhysicalExamState = {
-  vitals: PhysicalExamVitals;
-  components: PhysicalExamComponent[];
-};
-
-const initialState: PhysicalExamState = {
-  vitals: {
-    bloodPressure: "120 / 80",
-    heartRate: 70,
-    oxygenSaturation: 95.0,
-    temperature: 101.5,
-    respiratoryRate: 1,
-    bmi: 21,
-  },
-  components: [
-    {
-      bodyPart: "Head",
-      status: "Normocephalic",
-      custom: false,
-      editable: false,
-    },
-    {
-      bodyPart: "Eyes",
-      status: "Accommodate to light",
-      custom: false,
-      editable: false,
-    },
-  ],
-};
-
-function reducer(
-  state: PhysicalExamState,
-  action: { type: string; bodyPart: string }
-): PhysicalExamState {
-  let newState: PhysicalExamState = JSON.parse(JSON.stringify(state));
-  let successfulAction = false;
-
-  switch (action.type) {
-    case "newBodyPart":
-      newState.components.push({
-        bodyPart: "",
-        status: "",
-        custom: true,
-        editable: true,
-      });
-      successfulAction = true;
-      break;
-    case "makeEditable":
-      newState.components.forEach(c => {
-        if (c.bodyPart === action.bodyPart) {
-          c.status = "";
-          c.editable = true;
-          successfulAction = true;
-        }
-      });
-      break;
-    default:
-      break;
-  }
-
-  // This is primarily in case a mistake is made and action is given to a body
-  // part that does not exist (ex: someone wants to edit an `Eyes` body part
-  // but no such one exists). This should only be triggered on developer error.
-  if (!successfulAction) {
-    throw new Error("Action was missing required elements.");
-  }
-
-  return newState;
-}
-
-// Converts a name to a usable ID name. This is needed for when the body part
-// that comes from the database has to have a name. Note: This does not
-// guarantee uniqueness. It is up to the caller to make sure of this.
-// TODO: This might have to be removed since it may end up being pointless.
-function nameToID(name:string) {
-  return name.toLowerCase().replace(" ", "-");
-}
-
-function createFindingFromComponent(dispatch:physicalExamDispatch, c: PhysicalExamComponent) {
-  if (c.status !== "") {
-    return <b>{c.status}</b>;
-  }
-
-  // TODO: Note that the bodyPart is probably empty, which affects the ID.
-  return (
-    <PatientFormInput
-      dispatch={dispatch}
-      id={nameToID(c.bodyPart)}
-      inputType={"text"}
-      inputVal={""}
-      placeholder={"Enter text here"}
-      title={""}
-      isShowingCanvas={true}
-      isShowingText={false}
-      setIsShowingCanvas={() => {}}
-      setIsShowingText={() => {}}
-      canvasHeight={200}
-      canvasWidth={800}
-      isTextArea={true}
-    />
-  );
-}
-
-function addNewFinding(
-  dispatch: physicalExamDispatch,
-  physicalComp: PhysicalExamComponent
-) {
-  dispatch({ type: "makeEditable", bodyPart: physicalComp.bodyPart });
-}
-
-function addPhysicalComponent(dispatch: physicalExamDispatch) {
-  dispatch({ type: "newBodyPart", bodyPart: "" });
-}
-
-function getBodyPartCell(
-  dispatch: physicalExamDispatch,
-  physicalComp: PhysicalExamComponent
-) {
-  if (physicalComp.custom) {
-    // TODO: Note that the bodyPart can be empty, which may affect the ID.
-    return (
-      <span>
-        <PatientFormInput
-          dispatch={dispatch}
-          id={nameToID(physicalComp.bodyPart)}
-          inputType={"text"}
-          inputVal={""}
-          placeholder={"Enter text here"}
-          title={""}
-          isShowingCanvas={true}
-          isShowingText={false}
-          setIsShowingCanvas={() => {}}
-          setIsShowingText={() => {}}
-          canvasHeight={200}
-          canvasWidth={400}
-          isTextArea={true}
-        />
-      </span>
-    );
-  }
-
-  return <span>{physicalComp.bodyPart}</span>;
-}
-
-function getVitalTableRow(name: string, value?: any, unit?: string) {
-  if (value) {
-    return (
-      <tr>
-        <td>{name}</td>
-        <td>
-          {value} {unit || ""}
-        </td>
-      </tr>
-    );
-  }
-
-  return <></>;
-}
+type vitals = {[key:string]: field_value}
+type general = {[key:string]: field_value}
+type heent = {[key:string]: field_value}
+type cn_ii = {[key:string]: field_value}
+type cn_iii = {[key:string]: field_value}
+type cn_v = {[key:string]: field_value}
+type cn_vii = {[key:string]: field_value}
+type cn_viii = {[key:string]: field_value}
+type cn_ix = {[key:string]: field_value}
+type cn_xi = {[key:string]: field_value}
+type cn_xii = {[key:string]: field_value}
+type neck = {[key:string]: field_value}
+type pulmonary = {[key:string]: field_value}
+type cardiac = {[key:string]: field_value}
+type abdomen = {[key:string]: field_value}
+type skin = {[key:string]: field_value}
+// TODO : check if this is right
+type motor = {[key:string]: field_value}
+type coordination = {[key:string]: field_value}
+type gait = {[key:string]: field_value}
+type mental = {[key:string]: field_value}
 
 export const PhysicalExaminationPage: IndividualPatientProfile = ({
   pageName,
@@ -203,6 +37,7 @@ export const PhysicalExaminationPage: IndividualPatientProfile = ({
   setCurrentPage,
   transitionDuration,
   transitionName,
+  isShowingSidebar,
   patientID,
 }) => {
   const history = useHistory();
@@ -213,8 +48,6 @@ export const PhysicalExaminationPage: IndividualPatientProfile = ({
     }
   }, [currentPage]);
 
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
     <>
       <CSSTransition
@@ -224,90 +57,9 @@ export const PhysicalExaminationPage: IndividualPatientProfile = ({
         onEnter={() => setCurrentPage(pageName)}
         classNames={transitionName}
       >
-        <div className="physical-examination-history-page-outermost-container patient-profile-window">
-          <div className="patient-profile-page-title">
-            <h1>{pageName}</h1>
-            <table
-              id="physical-exam-vitals-table"
-              className="physical-exam-table"
-            >
-              <thead>
-                <tr>
-                  <td>Vital</td>
-                  <td>Value</td>
-                </tr>
-              </thead>
-              <tbody>
-                {getVitalTableRow("Blood Pressure", state.vitals.bloodPressure)}
-                {getVitalTableRow("Heart Rate", state.vitals.heartRate, "bpm")}
-                {getVitalTableRow(
-                  "Oxygen Saturation",
-                  state.vitals.oxygenSaturation,
-                  "%"
-                )}
-                {getVitalTableRow("Temperature", state.vitals.temperature, "F")}
-                {getVitalTableRow(
-                  "Respiratory Rate",
-                  state.vitals.respiratoryRate,
-                  "(unit)"
-                )}
-                {getVitalTableRow(
-                  "Oxygen Supplement",
-                  state.vitals.oxygenSupplement
-                )}
-                {getVitalTableRow("Weight", state.vitals.weight)}
-                {getVitalTableRow("Height", state.vitals.height)}
-                {getVitalTableRow("BMI", state.vitals.bmi)}
-                {getVitalTableRow("Pain Score", state.vitals.painScore)}
-              </tbody>
-            </table>
-            <br />
-            <br />
-            <table
-              id="physical-exam-findings-table"
-              className="physical-exam-table"
-            >
-              <thead>
-                <tr>
-                  <td className="physical-exam-table-right-align">Body Part</td>
-                  <td>Value</td>
-                  <td>Edit</td>
-                </tr>
-              </thead>
-              <tbody>
-                {state.components.map(c => {
-                  return (
-                    <tr key={c.bodyPart}>
-                      <td className="physical-exam-table-right-align">
-                        {getBodyPartCell(dispatch, c)}
-                      </td>
-                      <td
-                        className={
-                          "physical-exam-findings-value" +
-                          (!c.editable ? " physical-exam-grayed-out" : "")
-                        }
-                      >
-                        {createFindingFromComponent(dispatch, c)}
-                      </td>
-                      <td>
-                        {!c.editable && (
-                          <button onClick={e => addNewFinding(dispatch, c)}>
-                            Customize
-                          </button>
-                        )}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-            <button
-              className="physical-exam-button"
-              onClick={e => addPhysicalComponent(dispatch)}
-            >
-              Add Physical Component
-            </button>
-          </div>
+        <div className={isShowingSidebar ? "physical-examination-history-page-outermost-container patient-profile-window" :
+                                           "physical-examination-history-page-outermost-container patient-profile-window width-100"}>
+          
           <div className="patient-profile-nav-btns">
             <div className="nav-btn" style={{ right: "20px", top: "70px", position: "fixed", borderRadius: "5px" }} onClick={() => {
               // TODO : add POST request function here

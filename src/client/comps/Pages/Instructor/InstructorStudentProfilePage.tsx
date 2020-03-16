7111352ded33aa44412209d617e42601213b78b1
Chris <38708476+wchrisk@users.noreply.github.com>
2020-03-16 15:47:44 -0400

    Instructor student profile page operational now

--- src/client/comps/Pages/Instructor/InstructorStudentProfilePage.tsx
+++ src/client/comps/Pages/Instructor/InstructorStudentProfilePage.tsx
@@ -1,34 +1,68 @@
 import React, { useState } from "react";
+import "../../../scss/instructor/instructor-student-profile-page.scss";
 import { Header } from "../../SubComponents/Header";
+import { InstructorStudentPatientEncounter } from "../../SubComponents/Instructor/InstructorStudentPatientEncounter";
+import { max } from "../../../utils/utils";
 
 interface InstructorStudentProfilePageProps {
 }
 
 interface InstructorStudentProfile {
-  name: string;
-  pictureURL: string;
+  date: Date,
+  patientFirstName: string;
+  patientLastName: string;
 }
 
 const mockData: InstructorStudentProfile[] = [
+  { date: new Date(2827489172), patientFirstName: 'Justin', patientLastName: 'Trudeau' },
+  { date: new Date(0), patientFirstName: 'Billy', patientLastName: 'Bicep' },
+  { date: new Date(3333333333), patientFirstName: 'Panda', patientLastName: 'Monium' },
+  { date: new Date(1827489172), patientFirstName: 'Steven', patientLastName: 'Kang' },
 ];
 
 export const InstructorStudentProfilePage: React.FC<InstructorStudentProfile> = (
   props: InstructorStudentProfilePageProps
 ) => {
   const [isAvatarPopup, setIsAvatarPopup] = useState(false);
-  const [searchVal, setSearchVal] = useState("");
+  const [studentName, setStudentName] = useState("StudentName");
+  const [encounters, setEncounters] = useState(mockData);
 
   return <>
     <Header
       placeholder={""}
       isAvatarPopup={isAvatarPopup}
       setIsAvatarPopup={setIsAvatarPopup}
-      showSearch={true}
-      searchValue={searchVal}
-      setSearchValue={setSearchVal}
+      showSearch={false}
     />
-    <div className="">
-      Instructor student profile page
+    <div className="instructor-student-profile-container">
+      <div className="instructor-student-profile-title">
+        {studentName}'s Profile
+      </div>
+      <div>
+        <button className="instructor-student-profile-page-button" onClick={() => alert('TODO')}>
+          Contact Student
+        </button>
+        <button className="instructor-student-profile-page-button" onClick={() => alert('TODO')}>
+          Unlock Help/Tips
+        </button>
+      </div>
+      <div>
+        <div className="home-page-separator-line"/>
+        <div className="home-page-content">
+          {encounters.map((encounter) => {
+            return <InstructorStudentPatientEncounter
+              date={encounter.date}
+              firstName={encounter.patientFirstName}
+              lastName={encounter.patientLastName}
+            />
+          })}
+          <div
+            className="home-page-content-whitespace"
+            style={{ height: max(window.innerHeight - 400, 0) }}
+          >
+          </div>
+        </div>
+      </div>
     </div>
   </>;
 };

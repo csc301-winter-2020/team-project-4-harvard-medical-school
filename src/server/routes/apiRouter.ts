06e72cfcb08493ac89bdc5c64372e3ea2a08ec03
will <will.qie@mail.utoronto.ca>
2020-03-21 00:03:22 -0400

    add backend support

--- src/server/routes/apiRouter.ts
+++ src/server/routes/apiRouter.ts
@@ -747,4 +747,67 @@ router.get(
   }
 );
 
+
+const vision: any = require("@google-cloud/vision");
+const vision_client = new vision.ImageAnnotatorClient();
+const https: any = require('axios')
+
+function age_helper(age: number) {
+  if (age <= 1) {
+    return 1;
+  } else if (age <= 5) {
+    return 3;
+  } else if (age <= 16) {
+    return 4;
+  } else if (age <= 29) {
+    return 7;
+  } else if (age <= 39) {
+    return 5;
+  } else {
+    return 8;
+  }
+}
+
+function gender_helper(tmp: string) {
+  if (tmp === "Male") {
+    return 'm';
+  } else {
+    return 'f';
+  }
+}
+
+router.post(
+  "/api/analysis/:profile_id", 
+  async (req: Request, res: Response, next: NextFunction) => {
+    const image_payload: string = req.body['image'];
+    const request: any = {
+      "image": {
+        "content": image_payload
+      },
+      "features": [
+        {
+          "type": "DOCUMENT_TEXT_DETECTION"
+        }
+      ]
+    }
+    const [result] = await vision_client.annotateImage(request);
+    console.log(result.fullTextAnnotation.text);
+    const text: string = result.fullTextAnnotation.text;
+    const profile_id: number = parseInt(req.params.profile_id);
+    const db_res = await pool.query('SELECT age, gender_at_birth, pregnant FROM csc301db.patient_profile WHERE id = $1', [profile_id]);
+    if (db_res.rowCount === 0) {
+      res.status(404).send();
+    }
+    const age: number = age_helper(db_res.rows[0].age);
+    const gender: string = gender_helper(db_res.rows[0].gender_at_birth);
+    // TODO: FIX THIS LATER
+    const pregnant: string = "n";
+    const isbell_url: string = `https://apisandbox.isabelhealthcare.com/v2/ranked_differential_diagnoses?specialties=28&dob=${age}&sex=${gender}&pregnant=${pregnant}&region=10&querytext=${text}&suggest=suggest+differential+diagnosis&flag=sortbyrw_advanced&searchtype=0&web_service=json&callback=diagnosiscallback&authorization=urOSKOJyYvIOj8BnIgBwJI0KgXT4BR9VYShRyAPDdbcChStimoHWbUE6ILUM0Z4S`;
+    const isbell_res: any = await https.get(isbell_url);
+    const final_result: string = isbell_res.data.slice(18, isbell_res.data.length-2);
+    // console.log(JSON.parse(final_result));
+    res.status(200).json(JSON.parse(final_result));
+  }
+);
+
 export default router;

a9cd7c90cc4847efccbc9fef1660ce3c04a616d5
will <will.qie@mail.utoronto.ca>
2020-03-22 18:12:58 -0400

    fix bug that a empty json was not sent when error occurs

--- src/server/routes/apiRouter.ts
+++ src/server/routes/apiRouter.ts
@@ -1195,7 +1195,7 @@ router.post(
       [profile_id]
     );
     if (db_res.rowCount === 0) {
-      res.status(404).send();
+      res.status(404)..json({});
     }
     const age: number = age_helper(db_res.rows[0].age);
     const gender: string = db_res.rows[0].gender_at_birth === "Male" ? "m" : "f";
@@ -1233,13 +1233,13 @@ router.get(
     try {
       const result: any = await pool.query(query_string, [profile_id]);
       if (result.rowCount === 0) {
-        res.status(404).send();
+        res.status(404).json({});
       } else {
         res.status(200).json(result.rows[0]);
       }
     } catch (err) {
       console.log(err);
-      res.status(400).send();
+      res.status(400).json({});
     }
   }
 );

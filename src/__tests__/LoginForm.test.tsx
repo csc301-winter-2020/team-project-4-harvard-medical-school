f7a6ceb1a48ff68780874f4ed4d0b3b80eac6156
marcokch <chris.marcok@gmail.com>
2020-04-05 00:22:08 -0400

    remove test suite

--- src/__tests__/LoginForm.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, fireEvent, waitForElement } from "@testing-library/react";
-
-import { LoginPage } from "../client/comps/Pages/LoginPage";
-import React from "react";
-
-describe("<LoginPage />", () => {
-  test("should display a blank login form", async () => {});
-});

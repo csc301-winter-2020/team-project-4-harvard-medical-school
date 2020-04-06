04e77943bd77c89322d552a1b3c6e7bdc183cebb
marcokch <chris.marcok@gmail.com>
2020-04-06 00:28:35 -0400

    Instructors can now view their final diagnosis they set in the list view section

--- src/client/comps/Pages/PatientProfile/AssessmentAndPlanPage.tsx
+++ src/client/comps/Pages/PatientProfile/AssessmentAndPlanPage.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState, useReducer } from "react";
 import { CSSTransition } from "react-transition-group";
-import { IndividualPatientProfile, fetchAllCanvases } from "./PatientProfilePage";
+import {
+  IndividualPatientProfile,
+  fetchAllCanvases,
+} from "./PatientProfilePage";
 import { PatientFormInput } from "../../SubComponents/PatientProfile/PatientFormInput";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useHistory } from "react-router";
@@ -11,7 +14,12 @@ import { MyToast } from "../../../utils/types";
 
 function reducer(
   state: Assessment_State,
-  action: { type: string; fieldName?: string; value?: string; newState?: {[key: string]: string |boolean|number|null} }
+  action: {
+    type: string;
+    fieldName?: string;
+    value?: string;
+    newState?: { [key: string]: string | boolean | number | null };
+  }
 ): Assessment_State {
   switch (action.type) {
     case "field":
@@ -32,13 +40,11 @@ function reducer(
 type Assessment_State = {
   assessment: string;
   assessmentCanvas?: string;
-}
+};
 
 const initialState: Assessment_State = {
   assessment: "",
 };
-
-
 
 var allAttributes: any;
 
@@ -55,57 +61,58 @@ export const AssessmentAndPlanPage: IndividualPatientProfile = ({
   userType,
   templateId,
 }) => {
-
   async function saveData(url: string, state: any) {
-    console.log(state)
+    console.log(state);
     allAttributes.assessments = state.assessment;
-    if (state.assessmentCanvas !== undefined) allAttributes.assessments_canvas = state.assessmentCanvas;
+    if (state.assessmentCanvas !== undefined)
+      allAttributes.assessments_canvas = state.assessmentCanvas;
     allAttributes.class_id = classID;
     allAttributes.template_id = templateId;
     const res = await postData(url, allAttributes);
-    return await res.message
+    return await res.message;
   }
-  
+
   const [state, dispatch] = useReducer(reducer, initialState);
   const [lastState, setLastState] = useState(state);
 
   const [showingAssessmentCanvas, setShowingAssessmentCanvas] = useState(true);
   const [showingAssessmentText, setShowingAssessmentText] = useState(false);
-
+  const [finalDiagnosis, setFinalDiagnosis] = useState<string>(null);
   const { assessment, assessmentCanvas } = state;
 
-  const myToast:MyToast = toast as any;
+  const myToast: MyToast = toast as any;
 
   const history = useHistory();
   useEffect(() => {
-    if (currentPage === pageName){
+    if (currentPage === pageName) {
       document.title = `Patient Profile: ${pageName}`;
       history.push(`/patient/${patientID}/assessment`);
 
       // Get request
-      const url = '/api/patientprofile/' + patientID;
+      const url = "/api/patientprofile/" + patientID;
       fetch(url)
         .then((res) => {
-          return res.json()
+          return res.json();
         })
         .then((jsonResult) => {
-          console.log("Get Assessment")
-          console.log(jsonResult)
-          console.log(jsonResult.assessments_canvas)
+          console.log("Get Assessment");
+          console.log(jsonResult);
+          console.log(jsonResult.assessments_canvas);
+          setFinalDiagnosis(jsonResult.final_diagnosis);
           return fetchAllCanvases(jsonResult);
         })
-        .then(jsonResult => {
+        .then((jsonResult) => {
           allAttributes = jsonResult;
-          dispatch({ 
-            type: "many_fields", 
-            newState:{
+          dispatch({
+            type: "many_fields",
+            newState: {
               assessment: jsonResult.assessments,
-              assessmentCanvas: jsonResult.assessments_canvas
-            }
+              assessmentCanvas: jsonResult.assessments_canvas,
+            },
           });
         })
         .catch((error) => {
-          console.log("An error occured with fetch:", error)
+          console.log("An error occured with fetch:", error);
         });
     }
   }, [currentPage]);
@@ -116,7 +123,6 @@ export const AssessmentAndPlanPage: IndividualPatientProfile = ({
 
     setShowingAssessmentCanvas(canvasShow);
     setShowingAssessmentText(textShow);
-
   }, [defaultMode]);
 
   useEffect(() => {
@@ -126,18 +132,25 @@ export const AssessmentAndPlanPage: IndividualPatientProfile = ({
     }
 
     const timer = setTimeout(() => {
-      if (userType === "Student" && currentPage == pageName && state && state !== lastState) {
+      if (
+        userType === "Student" &&
+        currentPage == pageName &&
+        state &&
+        state !== lastState
+      ) {
         console.log(lastState);
         console.log(state);
 
-        saveData("/api/patientprofile/" + patientID, state).then((data) => {
-          console.log(data);
-          myToast.success('Autosaved.', {
-            autoClose: 1000,
+        saveData("/api/patientprofile/" + patientID, state)
+          .then((data) => {
+            console.log(data);
+            myToast.success("Autosaved.", {
+              autoClose: 1000,
+            });
+          })
+          .catch((err) => {
+            myToast.warn("Autosave failed.");
           });
-        }).catch((err) => {
-          myToast.warn('Autosave failed.');
-        });
 
         setLastState(state);
       }
@@ -145,7 +158,7 @@ export const AssessmentAndPlanPage: IndividualPatientProfile = ({
 
     return () => clearTimeout(timer);
   }, [state, lastState]);
-  
+
   return (
     <>
       <CSSTransition
@@ -155,7 +168,13 @@ export const AssessmentAndPlanPage: IndividualPatientProfile = ({
         onEnter={() => setCurrentPage(pageName)}
         classNames={transitionName}
       >
-        <div className={ isShowingSidebar ? "patient-profile-window" : "patient-profile-window width-100"}>
+        <div
+          className={
+            isShowingSidebar
+              ? "patient-profile-window"
+              : "patient-profile-window width-100"
+          }
+        >
           <div className="patient-profile-page-title">
             <h2>{pageName}</h2>
           </div>
@@ -177,25 +196,42 @@ export const AssessmentAndPlanPage: IndividualPatientProfile = ({
               isTextArea={true}
             />
           </div>
+          {userType !== "Student" && (
+            <div style={{ marginLeft: "100px", marginRight: "100px" }}>
+              <span className="bold-span">Instructor's Final Diagnosis:</span>{" "}
+              {finalDiagnosis === null || finalDiagnosis === ""
+                ? "The instructor has not set a final diagnosis. To do so, go back to the patient profile selection page and click the red 'i' on the right of the desired patient profile. Then click the pencil button near the bottom to edit the final diagnosis. Lastly, use the checkmark to save the final diagnosis."
+                : finalDiagnosis}
+            </div>
+          )}
           <div className="form-whitespace">
             <div className="home-page-content-whitespace-logo"></div>
           </div>
-          {
-            userType === "Student" && <div className="patient-profile-nav-btns">
-            <div className="nav-btn" style={{ right: "20px", top: "70px", position: "fixed", borderRadius: "5px" }} onClick={() => {
-              // TODO : add POST request function here
-              saveData('/api/patientprofile/' + patientID, state).then((data) => {
-                console.log(data)
-                myToast.success('Information saved')
-              }).catch((err) => {
-                myToast.success('Information could not be saved')
-              })
-            }}>
-              <FontAwesomeIcon icon="save" size="2x" />
+          {userType === "Student" && (
+            <div className="patient-profile-nav-btns">
+              <div
+                className="nav-btn"
+                style={{
+                  right: "20px",
+                  top: "70px",
+                  position: "fixed",
+                  borderRadius: "5px",
+                }}
+                onClick={() => {
+                  saveData("/api/patientprofile/" + patientID, state)
+                    .then((data) => {
+                      console.log(data);
+                      myToast.success("Information saved");
+                    })
+                    .catch((err) => {
+                      myToast.success("Information could not be saved");
+                    });
+                }}
+              >
+                <FontAwesomeIcon icon="save" size="2x" />
+              </div>
             </div>
-          </div>
-          }
-          
+          )}
         </div>
       </CSSTransition>
     </>
